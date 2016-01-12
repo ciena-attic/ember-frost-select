6b5e9c9c186447c068427a661049d784142e9b7f
Chris Stoll <chris.stoll@cyaninc.com>
2016-01-11 16:49:02 -0800

    first pass of functionality, drop-down with filtering and scroll

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,14 +1,18 @@
-/*jshint node:true*/
 /* global require, module */
-var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+var EmberApp = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
-    // Add options here
+  var app = new EmberApp(defaults, {
+	  sassOptions: {
+		  includePaths: [
+			  'bower_components/bourbon/app/assets/stylesheets',
+			  'node_modules/frost-css/styles'
+		  ]
+	  }
   });
 
   /*
-    This build file specifies the options for the dummy test app of this
+    This build file specifes the options for the dummy test app of this
     addon, located in `/tests/dummy`
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+/* globals module */
 /* jshint node: true */
+
 'use strict';
 
 module.exports = {
-  name: 'frost-select-2'
+	name: 'frost-select-2',
+
+	included: function(app) {
+		this._super.included(app);
+	}
 };
